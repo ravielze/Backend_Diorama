65b1c8c5f9ff04149aaa5ec3124d015d6f76b8d9
ravielze <ravielz20@gmail.com>
2022-02-05 11:24:16 +0700

    Update health check

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -16,17 +16,22 @@ public class HealthController : ControllerBase
     [HttpGet]
     public HealthDTO Get()
     {
+        TimeSpan diff = DateTime.Now.Subtract(HealthDTO.StartTime);
         return new HealthDTO
         {
             Date = DateTime.Now,
-            ServiceName = "Diorama-Backend"
+            ServiceName = "Diorama-Backend",
+            UpTime = String.Format("{0}h {1}m {2}s", diff.Hours, diff.Minutes, diff.Seconds)
         };
     }
 }
 
+
 public class HealthDTO
 {
+    public static readonly DateTime StartTime = DateTime.Now;
     public DateTime Date { get; set; }
+    public String UpTime { get; set; } = "0:0:0.0";
 
     public string ServiceName { get; set; } = "unnamed";
 
